e0b52163ab7861cd7936609956ab0369bd2abce1
ralarconb <ralarconbazan@gmail.com>
2022-02-06 17:47:32 -0500

    .

---
+++ src/test/java/com/abhs/chainsaw/ChainsawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhs.chainsaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChainsawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
